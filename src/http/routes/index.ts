42dd958048d42fa43846da6aa4275527f6b7816b
Guilherme Gonçalves dos Santos <guilhermegufes@gmail.com>
2022-12-26 17:50:50 -0300

    update

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -5,5 +5,8 @@ import userRouter from './user.routes';
 const routes = Router();
 
 routes.use('/user', userRouter);
+routes.use('/', async (request, response) => {
+  return response.json({});
+});
 
 export default routes;
